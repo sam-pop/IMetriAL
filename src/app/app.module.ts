fd1a9c395e50b517e0ba2d2df372f191cabf3747
sam-pop <poplovitch.samuel@gmail.com>
2018-06-22 00:12:57 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TempComponent } from './temp/temp.component';
+import { WeightComponent } from './weight/weight.component';
+import { VolumeComponent } from './volume/volume.component';
+import { LengthComponent } from './length/length.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TempComponent,
+    WeightComponent,
+    VolumeComponent,
+    LengthComponent
   ],
   imports: [
     BrowserModule,
